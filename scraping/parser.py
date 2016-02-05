b0eecd33d126e54d040a3d8e307b5ddae0941527
bethdellea <bethdellea@gmail.com>
2016-02-05 18:09:58 -0500

    Nearly Processed
    Unique Words don't like us, apparently. We'll figure it out.

--- scraping/parser.py
+++ scraping/parser.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 import sqlite3
 import re
 from textblob import *
 from nltk.stem.porter import *
+
 
 
 class OKCdb(object):
@@ -75,6 +77,30 @@ class OKCdb(object):
         res = self.execute(sql)
         return self.cur.lastrowid
 
+    def insertAdjAdv(self, adjadv, idNum):
+        """Inserts the percent of the profile that is adjective or adverb into the database"""        
+        sql = """UPDATE Users  SET advAdjPct= '%s' WHERE Id='%s'""" % (adjadv, idNum)
+        res = self.execute(sql)
+        return self.cur.lastrowid
+
+    def insertUniqueWds(self, numWords, idNum):
+        """Inserts the number of unique words in the profile into the db"""
+        sql = """UPDATE Users  SET uniqueWords= '%s' WHERE Id='%s'""" % (numWords, idNum)
+        res = self.execute(sql)
+        return self.cur.lastrowid
+
+    def insertPolarity(self, pol, idNum):
+        """Inserts the polarity rating of the profile into the db"""
+        sql = """UPDATE Users  SET polarity= '%s' WHERE Id='%s'""" % (pol, idNum)
+        res = self.execute(sql)
+        return self.cur.lastrowid
+
+    def insertSubjectivity(self, subj, idNum):
+        """Inserts the number of unique words in the profile into the db"""
+        sql = """UPDATE Users  SET subjectivity= '%s' WHERE Id='%s'""" % (subj, idNum)
+        res = self.execute(sql)
+        return self.cur.lastrowid
+    
 def avgWordLen(wordList):
     sz = len(wordList)
     length = 0
@@ -120,7 +146,7 @@ def adj_adv(blob, wdcount):
     for word, pos in blob.tags:
         if pos in posTagList:
             out += 1
-    return out/wdcount
+    return (out/wdcount)
 
 
 def unique_words(tokens):
@@ -140,39 +166,82 @@ def sentiment_analysis(sentences):
     return polarity, subjectivity
 
 #the things we've already done but probably still want the functions for
+#leaving like this bc I had to call it earlier and we may need to do so again
 def doneThings(profileID, db):
-    '''from inside the for loop and if statement of doTheThing'''
-    tokens = blob.words
-    wordct = len(tokens)
-    print ("there are ",wordct," total words in this sample.")
-    returned = db.insertWCt(wordct, profileID)
-    print ("thanks for making me do all this work.")
-    avgWLen = avgWordLen(tokens)
-    print("the average length of a word in this sample is ", avgWLen)
-    returned2 = db.insertAvgWrdLen(avgWLen, profileID)
-    blobSent = blob.sentences
-    aSentLen = avgSentLen(blobSent)
-    eturned3 = db.insertAvgSentLen(aSentLen, profileID)
-    print("the average length of a sentence in this sample is ", aSentLen, " words")
-        
+    wordsSet = db.getText_byID(profileID)
+    myWords = ""
+    if(wordsSet):
+        for item in wordsSet:
+            myWords += item
+    else:
+        myWords = "none"
+
+    blob = TextBlob(myWords)
+   # print(blob)
+    if(len(myWords.split()) > 5 and blob.detect_language()== "en" ):
+        #and actualWords(myWords)
+        tokens = blob.words
+        wordct = len(tokens)
+        if(wordct > 3): #this pulls out the punctuation for us so it works around the empty!!
+            '''from inside the for loop and if statement of doTheThing'''
+            #tokens = blob.words
+            #wordct = len(tokens)
+            #print ("there are ",wordct," total words in this sample.")
+            returned = db.insertWCt(wordct, profileID)
+            #print ("thanks for making me do all this work.")
+            avgWLen = avgWordLen(tokens)
+            #print("the average length of a word in this sample is ", avgWLen)
+            returned2 = db.insertAvgWrdLen(avgWLen, profileID)
+            blobSent = blob.sentences
+            aSentLen = avgSentLen(blobSent)
+            eturned3 = db.insertAvgSentLen(aSentLen, profileID)
+            #print("the average length of a sentence in this sample is ", aSentLen, " words")
+
+#some profiles were different charsets and got stored as ???
+    #unfortunately they're long enough to get past the filter, so we need to try harder
+    #I (Beth) found a more eloquent way of doing this,leaving it here for posterity mostly
+def actualWords(wordsToCheck):
+    wordsToCheck = wordsToCheck.split()
+    totalAnum = 0
+    totalChars = 0
+    for word in wordsToCheck:
+        for char in word:
+            totalChars +=1
+            if(char.isalnum()):
+                totalAnum +=1
+    if (totalChars - totalAnum >= totalChars/2):
+        #if more than half of your profile is not a-z, 1-9, we don't want your data!
+        return False
+    return True
 
 #eventually have the data output to a .csv so excel can do work for us
 def doTheThing(profileID, db):
     print("\nworking on profile ", profileID)
     wordsSet = db.getText_byID(profileID)
     myWords = ""
-    for item in wordsSet:
-        myWords += item
+    if(wordsSet):
+        for item in wordsSet:
+            myWords += item
+    else:
+        myWords = "none"
+
     blob = TextBlob(myWords)
    # print(blob)
-    if(len(myWords) > 3 and blob.detect_language()== "en"):
-        
+    if(len(myWords.split()) > 5 and blob.detect_language()== "en" ):
+        #and actualWords(myWords)
         tokens = blob.words
         wordct = len(tokens)
-        adjadv_pct = adj_adv(blob, wordct)
-        pol, subj = sentiment_analysis(blob.sentences)
-        unique = unique_words(tokens)
-        # do something with these
+        if(wordct > 3): #this pulls out the punctuation for us so it works around the empty!!
+            adjadv_pct = adj_adv(blob, wordct)
+            sents = sentiment_analysis(blob.sentences)
+            pol = sents[0]
+            subj = sents[1]
+            unique = unique_words(tokens)
+            # do something with these
+            db.insertAdjAdv(adjadv_pct, profileID)
+            db.insertUniqueWds(unique, profileID)
+            db.insertPolarity(pol, profileID)
+            db.insertSubjectivity(subj, profileID)
                 
     else:
         print("this blob was either not in English or basically empty. NOT COOL.")
@@ -190,10 +259,10 @@ def main():
         #here's hoping
     
     
-    for i in range(1296, 1650): #1129-1650 still need to go #not the most responsive solution but idgaf
+    for i in range(1650, 1715):  #1650
 
         doTheThing(i, db)
-    #profile 1295 is mean.
+    
     
 
         
